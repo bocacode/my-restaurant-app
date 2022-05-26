b476a6af2d0530992e9f0a09b9e07886df6ed1d1
Todd Albert <todd@bocacode.com>
2022-05-26 13:47:01 -0400

    added details scene

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './src/scenes/Home';
+import Details from './src/scenes/Details';
 
 const Stack = createNativeStackNavigator()
 
@@ -9,7 +10,8 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Home" component={Home} options={{ title: 'Local Restaurants' }} />
+        <Stack.Screen name="Details" component={Details} />
       </Stack.Navigator>
       <StatusBar style="auto" />
     </NavigationContainer>
